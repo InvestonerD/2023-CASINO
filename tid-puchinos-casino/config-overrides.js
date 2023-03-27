3d40d6a50df5a984d1ffddd677c8c2f51bffe462
InvestonerD <106421738+InvestonerD@users.noreply.github.com>
2023-03-27 12:23:09 -0700

    update

--- tid-puchinos-casino/config-overrides.js
+++ tid-puchinos-casino/config-overrides.js
@@ -1,35 +1,45 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-    const fallback = config.resolve.fallback || {};
-    Object.assign(fallback, {
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "assert": require.resolve("assert"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify"),
-        "url": require.resolve("url")
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify"),
+    "url": require.resolve("url")
+  })
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
     })
-    config.resolve.fallback = fallback;
-    config.plugins = (config.plugins || []).concat([
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-            Buffer: ['buffer', 'Buffer']
-        })
-    ])
-    
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('babel-loader'),
-        options: {
-            presets: [
-                require.resolve('@babel/preset-env'),
-                require.resolve('@babel/preset-react')
-            ]
-        }
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
+  ])
 
-    return config;
+  // Solución para legacy-peer-deps
+  config.resolve = {
+    ...config.resolve,
+    fullySpecified: false,
+  };
+  config.resolveLoader = {
+    ...config.resolveLoader,
+    fullySpecified: false,
+  };
+
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [
+        require.resolve('@babel/preset-env'),
+        require.resolve('@babel/preset-react')
+      ]
+    }
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
 }
